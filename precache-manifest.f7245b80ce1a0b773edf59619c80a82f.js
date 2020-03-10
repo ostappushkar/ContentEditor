5a79be5470159349093e04d58f527996e2dfbb68
Ostap Pushkar <ostappushkar@MacBook-Pro-Ostap.local>
2020-03-10 23:47:52 +0200

    Updates

---
+++ precache-manifest.f7245b80ce1a0b773edf59619c80a82f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d47a97d889e14d74dc701c7234deed",
+    "url": "/content-editor/index.html"
+  },
+  {
+    "revision": "6ea9cabb4044f2932bb2",
+    "url": "/content-editor/static/css/2.d98816e9.chunk.css"
+  },
+  {
+    "revision": "05628aa7492753c387b9",
+    "url": "/content-editor/static/css/main.20d0d857.chunk.css"
+  },
+  {
+    "revision": "6ea9cabb4044f2932bb2",
+    "url": "/content-editor/static/js/2.c17358e2.chunk.js"
+  },
+  {
+    "revision": "8f4ed1f313ca740b8026596e1c8d7f0d",
+    "url": "/content-editor/static/js/2.c17358e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05628aa7492753c387b9",
+    "url": "/content-editor/static/js/main.25a458bb.chunk.js"
+  },
+  {
+    "revision": "359dd7a240ac6d600d89",
+    "url": "/content-editor/static/js/runtime-main.b579ecdd.js"
+  },
+  {
+    "revision": "c8ef0ae5bf43fa83a0adaf2dbc3758ac",
+    "url": "/content-editor/static/media/Categories.c8ef0ae5.jpg"
+  },
+  {
+    "revision": "205a7820948874ec8ccdce308de7eee6",
+    "url": "/content-editor/static/media/GPS.205a7820.jpg"
+  }
+]);
